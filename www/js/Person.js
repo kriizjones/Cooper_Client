4c42f414e84fd940b9d47753aec3b364292d5fe1
kriizjones <kristoffer.s.karlsson@hotmail.com>
2017-01-20 09:24:27 +0100

    added files to client

---
+++ www/js/Person.js
@@ -0,0 +1,25 @@
+function Person(attr) {
+    this.gender = attr.gender;
+    this.age = attr.age;
+}
+
+Person.prototype.assessCooper = function (distance) {
+    this.cooperMessage = cooperAssessmentOf(this, distance);
+};
+
+var person = new Person({
+    gender: 'Male',
+    age: 20
+});
+
+person.assessCooper(1400);
+console.log(person.cooperMessage);
+// > Poor
+
+person.assessCooper(1800);
+console.log(person.cooperMessage);
+// > Below average
+
+person.assessCooper(2300);
+console.log(person.cooperMessage);
+// > Average
